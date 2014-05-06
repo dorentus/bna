ae8a2545eab005071045e741210a3fb0e544425b
Zhang Yi <zhangyi.cn@gmail.com>
2014-05-07 00:10:19 +0800

    :yum: [ci skip]

--- spec/authenticator_spec.rb
+++ spec/authenticator_spec.rb
@@ -1,26 +1,26 @@
 describe "bnet-authenticator-load" do
   before do
-    @authenticator = Bnet::Authenticator.new 'CN-1402-1943-1283', '4202aa2182640745d8a807e0fe7e34b30c1edb23'
+    @🔐 = Bnet::Authenticator.new 'CN-1402-1943-1283', '4202aa2182640745d8a807e0fe7e34b30c1edb23'
   end
 
   it "has expected restoration code" do
-    @authenticator.restorecode.should == '4CKBN08QEB'
+    @🔐.restorecode.should == '4CKBN08QEB'
   end
 
   it "calculates token correctly at 1347279358" do
-    @authenticator.get_token(1347279358).should == ['61459300', 1347279360]
+    @🔐.get_token(1347279358).should == ['61459300', 1347279360]
   end
 
   it "calculates token correctly at 1347279359" do
-    @authenticator.get_token(1347279359).should == ['61459300', 1347279360]
+    @🔐.get_token(1347279359).should == ['61459300', 1347279360]
   end
 
   it "calculates token correctly at 1347279360" do
-    @authenticator.get_token(1347279360).should == ['75939986', 1347279390]
+    @🔐.get_token(1347279360).should == ['75939986', 1347279390]
   end
 
   it "calculates token correctly at 1370448000" do
-    @authenticator.get_token(1370448000).should == ['59914793', 1370448030]
+    @🔐.get_token(1370448000).should == ['59914793', 1370448030]
   end
 end
 
@@ -31,12 +31,12 @@ describe "bnet-authenticator-networking" do
   end
 
   it "can request for a new authenticator" do
-    authenticator = Bnet::Authenticator.request_authenticator :US
-    authenticator.region.should == :US
+    🔐 = Bnet::Authenticator.request_authenticator :US
+    🔐.region.should == :US
   end
 
   it "can restore an anthenticator" do
-    authenticator = Bnet::Authenticator.restore_authenticator 'CN-1402-1943-1283', '4CKBN08QEB'
-    authenticator.secret.should == '4202aa2182640745d8a807e0fe7e34b30c1edb23'
+    🔐 = Bnet::Authenticator.restore_authenticator 'CN-1402-1943-1283', '4CKBN08QEB'
+    🔐.secret.should == '4202aa2182640745d8a807e0fe7e34b30c1edb23'
   end
 end
